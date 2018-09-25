dea98fb86d3291c07b3bd2a8bce21b8139aa6250
Diana <dchumpitazm@gmail.com>
2018-09-25 14:20:19 -0500

    mapa: Agregar funcionalidad básica del mapa
    Sobre el ejemplo del astronauta del SDK de Mapbox para
    Unity se agregó lo siguiente:
    - Seguimiento de cámara
    - Cambiar la velocidad del animador
    - Soporte para rotación
    - Se empezó la funcionalidad de zoom aunque incompleta
    - Se configuró (redujo) la velocidad de giro de rotación
      de la cámara-
    - Y se realizó algunos cambios visuales a la escena

--- Assets/Mapbox/Examples/2_AstronautGame/AstronautGame/Astronaut/CharacterMovement.cs
+++ Assets/Mapbox/Examples/2_AstronautGame/AstronautGame/Astronaut/CharacterMovement.cs
@@ -1,3 +1,5 @@
+// MY CODE
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,11 +20,20 @@ namespace Mapbox.Examples
 
 		void Update()
 		{
+           
 			
 			if (_controller.enabled)// Because the mouse control script interferes with this script
 			{
 				return;
 			}
+
+             if(Target.position == this.transform.position)
+            {
+                CharacterAnimator.SetBool("IsWalking", false);
+                return;
+            }
+            myCamControl();
+            StartCoroutine(LookAtNextPos());
 
 			foreach (var item in Materials)
 			{
@@ -32,14 +43,47 @@ namespace Mapbox.Examples
 			var distance = Vector3.Distance(transform.position, Target.position);
 			if (distance > 0.1f)
 			{
-				transform.LookAt(Target.position);
+				transform.LookAt(Target.position); // girar el player hacia el punto objetivo (target)
 				transform.Translate(Vector3.forward * Speed);
-				CharacterAnimator.SetBool("IsWalking", true);
+
+                CharacterAnimator.SetBool("IsWalking", true);
+                
+           
 			}
 			else
 			{
 				CharacterAnimator.SetBool("IsWalking", false);
 			}
 		}
-	}
+
+
+        [Header("CameraSettings")]
+        [SerializeField]
+        Camera cam;
+        Vector3 previousPos = Vector3.zero;
+        Vector3 deltaPos = Vector3.zero;
+
+        void myCamControl()
+        {
+            deltaPos = Target.position - previousPos;
+            deltaPos.y = 0;
+            cam.transform.position = Vector3.Lerp(cam.transform.position, cam.transform.position + deltaPos, Time.time);
+            previousPos = Target.position;
+        }
+
+        IEnumerator LookAtNextPos()
+        {
+            Quaternion neededRotation = Quaternion.LookRotation(Target.position - this.transform.position);
+            Quaternion thisRotation = this.transform.localRotation;
+
+            float t = 0;
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime / 0.25f;
+                var rotationValue = Quaternion.Slerp(thisRotation, neededRotation, t);
+                this.transform.rotation = Quaternion.Euler(0, rotationValue.eulerAngles.y, 0);
+                yield return null;
+            }
+        }
+    }
 }
