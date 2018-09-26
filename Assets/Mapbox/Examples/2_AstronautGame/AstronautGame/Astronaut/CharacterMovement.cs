0523ca0bf727a87a8fcd7f38ddec14c84a3799e2
Diana <dchumpitazm@gmail.com>
2018-09-25 19:11:00 -0500

    add method to simulate editor player behavior

--- Assets/Mapbox/Examples/2_AstronautGame/AstronautGame/Astronaut/CharacterMovement.cs
+++ Assets/Mapbox/Examples/2_AstronautGame/AstronautGame/Astronaut/CharacterMovement.cs
@@ -1,3 +1,5 @@
+// MY CODE
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,11 +20,20 @@ namespace Mapbox.Examples
 
 		void Update()
 		{
+           
 			
 			if (_controller.enabled)// Because the mouse control script interferes with this script
 			{
 				return;
 			}
+
+             if(Target.position == this.transform.position)
+            {
+                CharacterAnimator.SetBool("IsWalking", false);
+                return;
+            }
+            myCamControl();
+            StartCoroutine(LookAtNextPos());
 
 			foreach (var item in Materials)
 			{
@@ -34,12 +45,45 @@ namespace Mapbox.Examples
 			{
 				transform.LookAt(Target.position); // girar el player hacia el punto objetivo (target)
 				transform.Translate(Vector3.forward * Speed);
-				CharacterAnimator.SetBool("IsWalking", true);
+
+                CharacterAnimator.SetBool("IsWalking", true);
+                
+           
 			}
 			else
 			{
 				CharacterAnimator.SetBool("IsWalking", false);
 			}
 		}
-	}
+
+
+        [Header("CameraSettings")]
+        [SerializeField]
+        Camera cam;
+        Vector3 previousPos = Vector3.zero;
+        Vector3 deltaPos = Vector3.zero;
+
+        void myCamControl()
+        {
+            deltaPos = Target.position - previousPos;
+            deltaPos.y = 0;
+            cam.transform.position = Vector3.Lerp(cam.transform.position, cam.transform.position + deltaPos, Time.time);
+            previousPos = Target.position;
+        }
+
+        IEnumerator LookAtNextPos()
+        {
+            Quaternion neededRotation = Quaternion.LookRotation(Target.position - this.transform.position);
+            Quaternion thisRotation = this.transform.localRotation;
+
+            float t = 0;
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime / 0.25f;
+                var rotationValue = Quaternion.Slerp(thisRotation, neededRotation, t);
+                this.transform.rotation = Quaternion.Euler(0, rotationValue.eulerAngles.y, 0);
+                yield return null;
+            }
+        }
+    }
 }
