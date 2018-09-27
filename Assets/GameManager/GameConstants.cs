d58b093c863a46686a5ce3eb1dd6068565c18ae3
Diana <dchumpitazm@gmail.com>
2018-09-26 20:19:23 -0500

    login: Agregar e integrar interfaz de login

---
+++ Assets/GameManager/GameConstants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameConstants {
+
+	public static string LOGIN_SCENE= "LoginScene";
+	public static string MAP_SCENE= "BestMapScene";
+
+
+}
