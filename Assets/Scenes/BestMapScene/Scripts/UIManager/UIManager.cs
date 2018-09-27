421deb4ef6409dfe8095a2d94eeee53b608f99aa
Diana <dchumpitazm@gmail.com>
2018-09-27 09:22:22 -0500

    UIManager para escena Mapa

---
+++ Assets/Scenes/BestMapScene/Scripts/UIManager/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	
+	[SerializeField]
+	private GameObject menu;
+
+	public void toogleMenu(){
+		menu.SetActive(!menu.activeSelf);
+	}
+
+	public void openProfile(){
+		//do something
+	}
+}
