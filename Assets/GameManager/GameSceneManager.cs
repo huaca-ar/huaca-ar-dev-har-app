607f86c0d121073066193b92498b9ca01360e4eb
Diana <dchumpitazm@gmail.com>
2018-10-04 12:58:58 -0500

    Agregar zona de excavacion

---
+++ Assets/GameManager/GameSceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadMapScene(){
+		SceneManager.LoadScene(GameConstants.MAP_SCENE,LoadSceneMode.Single);
+	}	
+	
+	public void LoadProfileScene(){
+		SceneManager.LoadScene(GameConstants.PROFILE_SCENE,LoadSceneMode.Single);
+	}
+
+	public void LoadExcavationScene(){
+		SceneManager.LoadScene(GameConstants.EXCAVATION_SCENE,LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scenes/ExcavationArea/Scripts/ExcavationSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	
+
+	public void LoadMapScene(){
+		SceneManager.LoadScene(GameConstants.MAP_SCENE,LoadSceneMode.Single);
+	}
+}
