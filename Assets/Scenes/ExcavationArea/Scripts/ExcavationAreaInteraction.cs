2838ede47434f9a883dd4840113e00a31c4ca9a3
Paul Aquino Vega <a20121669@pucp.pe>
2018-10-08 20:51:21 -0500

    Excavacion: se habilita toques para el prefab de zona de excavacion en la pantalla de excavacion

---
+++ Assets/Scenes/ExcavationArea/Scripts/ExcavationAreaInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcavationAreaInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseDown() {
+		Debug.Log("Zona de excavacion tocada");
+		Animator animator = gameObject.GetComponent<Animator>();
+		animator.SetBool("dig", true);
+	}
+}
