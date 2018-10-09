a969afc4f74eaa2f002b76974dd907b0c43abc9d
Diana <dchumpitazm@gmail.com>
2018-10-09 00:51:32 -0500

    Login: Auth con Firebase (nick y pass)

---
+++ Assets/GameManager/DBManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DBManager : MonoBehaviour {
+
+
+	const string SUFIX = "@arqueopucp.com";
+
+	private bool usable;
+
+	public InputField email, password;
+	// Use this for initialization
+	
+	void Start(){
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+			var dependencyStatus = task.Result;
+			if (dependencyStatus == Firebase.DependencyStatus.Available) {
+				// Set a flag here indiciating that Firebase is ready to use by your
+				// application.
+				usable = true;
+				
+			} else {
+				UnityEngine.Debug.LogError(System.String.Format(
+				"Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+				// Firebase Unity SDK is not safe to use here.
+				usable = false;
+			}
+		});	
+
+		Debug.Log(usable);
+	}
+
+	
+
+	public void LoginButtonPressed(){
+
+		if(usable){
+			string fakeEmail = email.text + SUFIX;
+			
+			Debug.Log(fakeEmail);
+
+			FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(fakeEmail,password.text).
+			ContinueWith((response)=>{
+				
+				if(response.IsCanceled){
+					Debug.LogError("LOGIN CANCELADO");
+				}
+				if(response.IsFaulted){
+					Debug.LogError("USUARIO O CONTRASEÑA INCORRECTOS");
+					SSTools.ShowMessage("Usuario o Contraseña incorrectos", SSTools.Position.bottom,SSTools.Time.twoSecond);
+				}
+
+				FirebaseUser user = response.Result;
+				Debug.LogFormat("Usuario logueado: {0}  {1}", user.Email, user.UserId);
+				SSTools.ShowMessage("Bienvenido",SSTools.Position.bottom,SSTools.Time.twoSecond);
+
+				GameSceneManager manager =  new GameSceneManager();
+				manager.LoadMapScene();
+			});
+
+		}
+		
+	
+		
+		
+	}
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool RegisterUser(){
+
+
+		if(usable){
+			
+			string fakeEmail = email.text + SUFIX;
+			Debug.Log(fakeEmail);
+
+			FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(fakeEmail,password.text).ContinueWith((response)=>{
+
+				if(response.IsFaulted){
+					return false;
+				}
+
+				if(response.IsCanceled){
+					return false;
+				}
+				
+				FirebaseUser newUser = response.Result;
+
+
+
+				string[] cadenas = newUser.Email.Split('@');
+
+				Debug.LogFormat("USERID: {0} NICK: {1}", newUser.UserId, cadenas[0]);
+
+				return true;
+			});
+		}
+		
+		
+		return false;
+
+	}
+}
---
+++ Assets/Scenes/NewLogin/Scripts/LoginUIManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginUIManager : MonoBehaviour {
+
+	// Use this for initialization
+	
+	[SerializeField]
+	private GameObject existingUser;
+	[SerializeField]
+	private GameObject newUser;
+
+	[SerializeField]
+	private GameObject nickname;
+
+	[SerializeField]
+	private GameObject password;
+
+	[SerializeField]
+	private GameObject nickWarning;
+
+	[SerializeField]
+	private GameObject passWarning;
+
+	[SerializeField]
+	private DBManager dBManager;
+
+	[SerializeField]
+	private Image logo;
+
+	[SerializeField]
+	private GameObject startgame;
+	[SerializeField]
+	private GameObject loginTitle;
+
+    private bool isValidData;
+
+    public bool IsValidData
+    {
+        get
+        {
+            return isValidData;
+        }
+
+        set
+        {
+            isValidData = value;
+        }
+    }
+
+    public void ExistingUserButtonPressed(){
+
+		// ocultar los botones actuales
+
+		existingUser.SetActive(false);
+		newUser.SetActive(false);
+
+		Vector3 position = logo.transform.position;
+		logo.SetNativeSize();
+		logo.transform.position = position + new Vector3(0,106,0);
+		
+
+		// activar botones de la vista iniciar sesion
+
+		nickname.SetActive(true);
+		password.SetActive(true);
+		startgame.SetActive( true);
+		loginTitle.SetActive(true);
+		
+	}
+
+
+	public void validateNickname(){
+		InputField myField = nickname.GetComponent<InputField>();
+
+
+		if(myField.text.Length==0){
+			SSTools.ShowMessage("Ingresar nickname",SSTools.Position.bottom,SSTools.Time.oneSecond);
+			IsValidData=false;
+			nickWarning.SetActive(true);
+			return ;
+		}
+
+		if(myField.text.Length<4){
+			IsValidData=false;
+			SSTools.ShowMessage("Nick muy corto",SSTools.Position.bottom,SSTools.Time.oneSecond);
+			nickWarning.SetActive(true);
+			return;
+		}
+		IsValidData=true;
+		nickWarning.SetActive(false);
+		
+	}
+	
+	public void validatePassword(){
+
+		InputField myField = password.GetComponent<InputField>();
+
+		if(myField.text.Length==0){
+			IsValidData=false;
+			SSTools.ShowMessage("Ingresar contraseña",SSTools.Position.bottom,SSTools.Time.oneSecond);
+			passWarning.SetActive(true);
+			return ;
+		}
+		if(myField.text.Length<6){
+			IsValidData=false;
+			SSTools.ShowMessage("Contraseña muy corta",SSTools.Position.bottom,SSTools.Time.oneSecond);
+			passWarning.SetActive(true);
+			return;
+		}
+
+		IsValidData=true;
+		passWarning.SetActive(false);
+
+	}
+
+	public void StartGameButtonPressed(){
+
+		// Debug.LogFormat("val nick: {0}  pass: {1}");
+
+		Debug.LogFormat("Es data valida {0}",isValidData.ToString());
+
+		if(IsValidData){			
+			dBManager.LoginButtonPressed();
+			passWarning.SetActive(false);
+			nickWarning.SetActive(false);
+			
+		}
+	
+		
+	}
+
+	
+
+}
