cbf77dd60962b7dd9941b868571c87fc1eccdf6f
Diana <dchumpitazm@gmail.com>
2018-10-11 08:32:57 -0500

    Map: mostrar avatar segun el genero escogido

---
+++ Assets/Scenes/BestMapScene/Scripts/UIManager/MapUIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapUIManager : MonoBehaviour {
+
+	// Use this for initialization
+	
+	[SerializeField] private GameObject menu;
+	[SerializeField] private GameObject femaleAvatar;
+	[SerializeField] private GameObject maleAvatar;
+
+	void Start(){
+		
+
+		if(PlayerPrefs.GetInt("gender") == 1){
+			maleAvatar.SetActive(true);
+		}else{
+			femaleAvatar.SetActive(true);
+		}
+	}
+	public void toogleMenu(){
+		menu.SetActive(!menu.activeSelf);
+	}
+
+	
+}
