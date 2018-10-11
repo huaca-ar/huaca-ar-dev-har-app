0c0495a7871632b734ca5f2880f0bea499872771
Diana <dchumpitazm@gmail.com>
2018-10-11 18:43:32 -0500

    profile: Colocar avatar en el centro del canvas

--- Assets/Scenes/Profile/Scripts/UIManager/ProfileScreenManager.cs
+++ Assets/Scenes/Profile/Scripts/UIManager/ProfileScreenManager.cs
@@ -8,10 +8,20 @@ public class ProfileScreenManager : MonoBehaviour {
 
 	// Use this for initialization
 	[SerializeField] private Text exp;
+	[SerializeField] private GameObject maleAvatar;
+	[SerializeField] private GameObject femaleAvatar;
 
 	void Start(){
 		exp.text = PlayerPrefs.GetInt("exp").ToString();
+
+
+		if(PlayerPrefs.GetInt("gender")==1){
+			maleAvatar.SetActive(true);
+		}else{
+			femaleAvatar.SetActive(true);
+		}
 	}
+
 	public void LoadMapScene(){
 		SceneManager.LoadScene(GameConstants.MAP_SCENE,LoadSceneMode.Single);
 	}
