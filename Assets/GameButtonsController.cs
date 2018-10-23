642e985e54716985004c358fc9c86e5b98e16abd
Paul Aquino Vega <a20121669@pucp.pe>
2018-10-23 10:11:14 -0500

    Excavacion: se agrega modalidad de excavacion AR, falta logica de excavacion para esta nueva pseudoescena

---
+++ Assets/GameButtonsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameButtonsController : MonoBehaviour {
+
+	public GameObject extractionCanvas;
+	public GameObject ARCamera;
+	public GameObject extractionZoneAR;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ARButton() {
+		Debug.Log("Se acaba de pulsar el boton de AR");
+		ARCamera.SetActive(!ARCamera.activeSelf);
+		extractionZoneAR.SetActive(!extractionZoneAR.activeSelf);
+		extractionCanvas.SetActive(!extractionCanvas.activeSelf);
+		//
+		setExcavationZoneAR();
+	}
+
+	public void setExcavationZoneAR() {
+		Debug.Log("Se inicia seteo de posicion de zona de excavacion AR");
+		//extractionZoneAR.transform.position = new Vector3(extractionZoneAR.transform.position.x, extractionZoneAR.transform.position.y, (Random.insideUnitSphere.z * 20.0f));
+		//Vector3 playerPosition = new Vector3(extractionZoneAR.transform.position.x, extractionZoneAR.transform.position.y, ARCamera.transform.position.z);
+		//extractionZoneAR.transform.LookAt(playerPosition);
+	}
+
+}
