aaa69269572a819e4d90527fb62ff64cf307fbf6
Diana <dchumpitazm@gmail.com>
2018-11-04 17:58:35 -0500

    Remover Firebase SDK y colocar support AAR de mapbox de nuevo

--- Assets/Scenes/BestMapScene/Scripts/UIManager/MapUIManager.cs
+++ Assets/Scenes/BestMapScene/Scripts/UIManager/MapUIManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Firebase.Database;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,86 +18,17 @@ public class MapUIManager : MonoBehaviour {
 	public GameObject transitionCanvas;
 
 
-	public delegate void MyFunction();
-	protected MyFunction callback;
 
 
 
 	void Start(){
-		// uuid = PlayerPrefs.GetString("userid");
-
-		// Debug.LogFormat("Entre al metodo updateLocalInfo uuid {0}", uuid);
-
-		// // isGenderAvailable(setActiveAvatar);
-		// DatabaseReference personalInfo = FirebaseDatabase.DefaultInstance.RootReference.Child("Users").Child(uuid).Child("PersonalInfo");
-
-		// personalInfo.Child("gender").GetValueAsync().ContinueWith((res)=>{
-
-
-		// 	if(res.IsCompleted){
-				
-		// 		Debug.Log("ya culmino");
-
-		// 		Debug.Log(res.Result.Value);
-		// 		gender = (int) res.Result.Value;
-		// 		PlayerPrefs.SetInt("gender",gender);
-		// 		Debug.Log("usar callback");
-
-		// 		setActiveAvatar(gender);
-		// 	}
-		// });
-
-		// StartCoroutine(waitFire);
-
+		
 
 		setActiveAvatar(PlayerPrefs.GetInt("gender"));
 
 	}
 
-	// void Update(){
-	// 	if(!femaleAvatar.activeSelf && !maleAvatar.activeSelf){
-	// 		setActiveAvatar(PlayerPrefs.GetInt("gender"));
-	// 	}
-	// }
-
-	// IEnumerator waitFire(){
-	// 	yield return new WaitForSeconds(2);
-	// 	setActiveAvatar(PlayerPrefs.GetInt("gender"));
-
-
-	// }
-	// void Awake(){
-	// 	isGenderAvailable(setActiveAvatar);
-		
-	// }
-
 	
-
-
-	// private void isGenderAvailable(Action<int> callback){
-	// 	DatabaseReference personalInfo = FirebaseDatabase.DefaultInstance.RootReference.Child("Users").Child(uuid).Child("PersonalInfo");
-
-	// 	personalInfo.Child("gender").GetValueAsync().ContinueWith((res)=>{
-
-
-	// 		if(res.IsCompleted){
-				
-	// 			Debug.Log(res.Result.Key);
-	// 			Debug.Log(res.Result.Value.ToString());
-
-	// 			Debug.Log("ya culmino");
-
-	// 			Debug.Log(res.Result.Value);
-	// 			gender = (int) res.Result.Value;
-	// 			PlayerPrefs.SetInt("gender",gender);
-	// 			Debug.Log("usar callback");
-	// 			callback(gender);
-
-	// 		}
-
-				
-	// 	});
-	// }
 
 	void Update() {
 		if (transitionAnim.GetBool("finishIntro"))
